53fb255aa8107ea6bba132dacf14bf479bd56c7b
刘东 <liudong-alex>
2020-05-08 22:06:27 +0800

    完善测试页面

--- pages/work/demo.js
+++ pages/work/demo.js
@@ -7,45 +7,79 @@ Page({
     ],
     itemB: [
       {name: '选项B', value: 'B'}
-    ]
+    ],
   },
   onLoad: function () {
     // 页面渲染后 执行
     this.setData({ msg: "投票" }); 
+    var countItem = wx.getStorageSync('countItem');
+    if (!countItem) {
+      wx.setStorage({
+        key: 'countItem',
+        data: {
+          countA:0,
+          countB:0
+        }
+      })
+    }
   },
   // radio事件
  radioChange: function (e) {
-  var msg = '';
-  if(e.detail.value== 'A'){
-    this.setData({
-      msg:'给A投票+1'
-    });
-    msg = '给A投票+1';
-  }else{
-    this.setData({
-      msg:'给B投票+1'
-    });
-    msg = '给B投票+1';
-  };
-  wx.showModal({
-    title: '提示',
-    content: msg,
-    success: function (res) {
-      if (res.confirm) {
-        //这里是点击了确定以后
-        console.log('用户点击确定');
-        wx.navigateTo({
-          url: '../index/index'
-        });
-      } else {
-        //这里是点击了取消以后
-        console.log('用户点击取消');
-        wx.navigateTo({
-          url: '../index/index'
-        });
+    var msg = '';
+    if(e.detail.value== 'A'){
+      this.setData({
+        msg:'给A投票+1'
+      });
+      msg = '给A投票+1';
+    }else{
+      this.setData({
+        msg:'给B投票+1'
+      });
+      msg = '给B投票+1';
+    };
+    this.setCount(e.detail.value);
+    var $this = this;
+    wx.showModal({
+      title: '提示',
+      content: msg,
+      success: function (res) {
+        if (res.confirm) {
+          //这里是点击了确定以后
+          console.log('用户点击确定');
+          wx.navigateTo({
+            url: '../work/demo1'
+          });
+        } else {
+          //这里是点击了取消以后
+          console.log('用户点击取消');
+          wx.navigateTo({
+            url: '../index/index'
+          });
+        }
       }
+    });
+},
+setCount:function(e) {
+  var $this = this;
+  var countA = Number($this.getCount().countA);
+  var countB = Number($this.getCount().countB);
+  if (e == 'A') {
+    countA = countA + 1;
+  } else {
+    countB = countB + 1;
+  }
+  wx.setStorage({
+    key: 'countItem',
+    data: {
+      countA: countA,
+      countB: countB
     }
   });
 },
+getCount:function(e) {
+  var countItem = wx.getStorageSync('countItem');
+  console.log(countItem);
+  return countItem;
+}
 
 })
