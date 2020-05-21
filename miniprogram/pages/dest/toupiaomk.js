87c59c3c59233686a92dcee9d562cb7533e1f747
刘东 <liudong1tcsl>
2020-05-21 21:11:29 +0800

    完善投票功能

--- miniprogram/pages/dest/toupiaomk.js
+++ miniprogram/pages/dest/toupiaomk.js
@@ -128,7 +128,8 @@ Page({
     var userInfo = this.common.util.getCache('userInfo');
     var db = this.common.util.getDB();
     db.collection('user_show').where({
-      user_open_id: userInfo.open_id
+      user_open_id: userInfo.open_id,
+      toupiao_list_id:toupiaoModel._id
     }).get().then(res => {
         if (res.data.length > 0) {
           wx.showModal({
